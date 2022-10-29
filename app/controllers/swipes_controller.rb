4813e8b4739d68cc74f5bd0e52313f3858c94649
Dylan Gnatz <dylangnatz@dylans-mbp.kpn>
2022-10-29 18:28:32 +0200

    updated MVC for swipes

--- app/controllers/swipes_controller.rb
+++ app/controllers/swipes_controller.rb
@@ -1,9 +1,17 @@
 class SwipesController < ApplicationController
   def index
+    if current_user
+      @user = current_user
+    else
+      redirect_to new_user_session_path, notice: 'You are not logged in.'
+    end
     @potential_match = Profile.find(Profile.ids.sample)
   end
 
   def create
-    @swipe = Swipe.create!(swipe_params)
+    user_id = @user.id
+    match_id = @potential_match.id
+
+    @swipe = Swipe.create!()
   end
 end
