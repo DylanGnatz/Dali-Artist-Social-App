304a5a1ced76a5ffc165ec7a879a786e525ec61f
MisterGado <ipod.alexisgado@gmail.com>
2022-10-30 15:29:24 -0400

    better routing from root

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,5 @@ Rails.application.routes.draw do
   resources :chats_messages
   resources :friends
 
-  devise_scope :user do
-    authenticated :user do
-      root 'profiles#index', as: :authenticated_root
-    end
-  
-    unauthenticated do
-      root 'devise/sessions#new', as: :unauthenticated_root
-    end
-  end
+  root :to => redirect('/profiles/index')
 end
