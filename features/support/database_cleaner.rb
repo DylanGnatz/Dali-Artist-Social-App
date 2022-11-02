e11d9496f90fe90db4f5a5faa78336dbf1be3dcc
MisterGado <ipod.alexisgado@gmail.com>
2022-11-02 16:03:24 -0400

    db clean bis

--- features/support/database_cleaner.rb
+++ features/support/database_cleaner.rb
@@ -1,6 +1,6 @@
 require 'database_cleaner/active_record'
 
-DatabaseCleaner.strategy = :truncation
+DatabaseCleaner.strategy = :transaction
 
 Around do |scenario, block|
   DatabaseCleaner.cleaning(&block)
