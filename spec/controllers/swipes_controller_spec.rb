4d01608716ba8528a09764ded2e8426de0e4546a
Dylan Gnatz <dylangnatz@Dylans-MacBook-Pro.local>
2022-11-02 14:19:20 +0100

    beginning to add swipe Rspec tests

---
+++ spec/controllers/swipes_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe SwipesController do
+  login_user
+
+  before :each do
+    @profile1 = @user.profile
+    @profile2 = create(:profile)
+    @profile3 = create(:profile)
+    
+  end
+
+  describe "#create" do
+    it "creates a new match record" do
+      @swipe = Swipe.create(profile_id: @profile1.id, swiped_id: @profile2.id, interested: true, swipe_time: Time.now())
+      @swipe.should be_an_instance_of Swipe
+    end
+  end
+end
