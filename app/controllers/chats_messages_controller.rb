3faa915d82fce7f115c9808fb119fa98b3e533f4
shehan360 <shehan360@icloud.com>
2022-11-01 22:44:37 -0400

    updates

--- app/controllers/chats_messages_controller.rb
+++ app/controllers/chats_messages_controller.rb
@@ -1,6 +1,10 @@
 class ChatsMessagesController < ApplicationController
+    before_action :authenticate_user!
+
     def create
-        msg = ChatsMessage.create!(chat_id: params[:chat_id], profile_id: params[:profile_id], msg: params[:chats_message][:msg])
+        profile = Profile.find_by(user_id: current_user.id)
+
+        msg = ChatsMessage.create!(chat_id: params[:chat_id], profile_id: profile.id, msg: params[:chats_message][:msg])
         msg.save!
         redirect_to chat_path(params[:chat_id])
     end
