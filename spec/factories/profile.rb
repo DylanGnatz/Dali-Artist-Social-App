403fa738520db5d1e24c371ee0ad311571d6b116
shehan360 <shehan360@icloud.com>
2022-11-02 20:36:06 -0400

    fix rspec errors after db reset

--- spec/factories/profile.rb
+++ spec/factories/profile.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
     factory :profile do
-        sequence(:id) { |n| n }
+        sequence(:id) { |n| Time.now() + n }
         sequence(:username) { |n| "user#{n}" }
         bio {"test man bio"}
         user { association :user, profile: instance }
