79628ec7db8158a7a2c954d7d297b32f3d43ab43
shehan360 <shehan360@icloud.com>
2022-11-02 21:22:24 -0400

    add empty chat message check

--- app/controllers/chats_messages_controller.rb
+++ app/controllers/chats_messages_controller.rb
@@ -4,8 +4,12 @@ class ChatsMessagesController < ApplicationController
     def create
         profile = Profile.find_by(user_id: current_user.id)
 
-        msg = ChatsMessage.create!(chat_id: params[:chat_id], profile_id: profile.id, msg: params[:chats_message][:msg])
-        msg.save!
+        if params[:chats_message][:msg] == ''
+            flash[:alert] = "The message was empty!"
+        else
+            msg = ChatsMessage.create!(chat_id: params[:chat_id], profile_id: profile.id, msg: params[:chats_message][:msg])
+            msg.save!
+        end
         redirect_to chat_path(params[:chat_id])
     end
 end
