bc2d80a1dd1febed7995a86670e15fa1ef8cd097
AlexisGado <ipod.alexisgado@gmail.com>
2022-11-15 09:35:05 -0500

    fix profile model

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,7 +4,6 @@ class ProfilesController < ApplicationController
     @profile = Profile.find_by(user_id: current_user.id)
   end
   def edit
-
     @profile = Profile.find_by(user_id: current_user.id)
     profile_tags = ProfileTag.where(profile_id: @profile.id).pluck(:tags_id)
     @tags = Tag.where(id: profile_tags)
