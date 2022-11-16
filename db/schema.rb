4f015855217db8efcd734ab45a50c8c245d15222
AlexisGado <ipod.alexisgado@gmail.com>
2022-11-15 20:29:53 -0500

    add timestamps to artworks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_15_164530) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_15_234640) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_15_164530) do
     t.string "link", limit: 100
     t.string "description", limit: 2000
     t.boolean "isPrivate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["profile_id"], name: "index_artworks_on_profile_id"
   end
 
