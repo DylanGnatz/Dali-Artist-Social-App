b93dc73eec0f699f80aa8d0d746a9099b6ac5fd9
Dylan Gnatz <gnatzd@outlook.com>
2022-12-02 20:28:33 +0000

    added update method for search settings

--- app/controllers/search_settings_controller.rb
+++ app/controllers/search_settings_controller.rb
@@ -4,4 +4,12 @@ class SearchSettingsController < ApplicationController
     @profile = Profile.find_by(user_id: current_user.id)
     @search_setting = @profile.search_setting
   end
+
+  def update
+    @search_setting = Profile.find_by(user_id: current_user.id).search_setting
+    @search_setting.update!(params)
+
+    flash[:notice] = "Profile was successfully updated."
+    redirect_to search_settings_path
+  end
 end
