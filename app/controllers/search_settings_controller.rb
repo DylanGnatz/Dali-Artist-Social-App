0b30f41fe6e15451ffc0bf46397d35df55dc2e0f
Dylan Gnatz <gnatzd@outlook.com>
2022-12-02 19:58:48 +0000

    added search_setting var to controller

--- app/controllers/search_settings_controller.rb
+++ app/controllers/search_settings_controller.rb
@@ -2,5 +2,6 @@ class SearchSettingsController < ApplicationController
   before_action :authenticate_user!
   def index
     @profile = Profile.find_by(user_id: current_user.id)
+    @search_setting = @profile.search_setting
   end
 end
